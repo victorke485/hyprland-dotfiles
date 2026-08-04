763f9c43fe8ad752865365112c680b0970796156
victorke485 <victorvickuat@gmail.com>
2026-08-04 22:09:00 +0300

    feat: update dotfiles

--- hyprland/.config/hypr/modules/autostart.lua
+++ hyprland/.config/hypr/modules/autostart.lua
@@ -3,9 +3,10 @@
 -------------------
 -- Autostart necessary processes (like notifications daemons, status bars, etc.)
 
-hl.on("hyprland.start", function ()
-   hl.exec_cmd("nm-applet")
-   hl.exec_cmd("awww-daemon")
-   hl.exec_cmd("hypridle")
-   hl.exec_cmd("ashell")
+hl.on("hyprland.start", function()
+	hl.exec_cmd("nm-applet")
+	hl.exec_cmd("awww-daemon")
+	hl.exec_cmd("hypridle")
+	hl.exec_cmd("ashell")
+	hl.exec_cmd("foot --server")
 end)
--- hyprland/.config/hypr/modules/monitors.lua
+++ hyprland/.config/hypr/modules/monitors.lua
@@ -10,3 +10,6 @@ hl.monitor({
     position = "auto",
     scale    = "1",
 })
+
+-- hl.monitor({ output = "eDP-1", mode = "1920x1080@60.01000", position = "0x0", scale = 1 })
+hl.monitor({ output = "HDMI-A-2", mode = "1920x1080@60.00", position = "0x0", scale = 1, mirror = "eDP-1" })
